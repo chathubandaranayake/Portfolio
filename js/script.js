6fc3ec286867fd62dabdbfa5603cb96f71821d66
chathubandaranayake <136330847+chathubandaranayake@users.noreply.github.com>
2024-03-03 23:56:22 -0800

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+var typed= new Typed(".textc", {
+    strings: ["an Undergraduate" , "a Graphic Designer" , "a Web Designer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true,
+});
